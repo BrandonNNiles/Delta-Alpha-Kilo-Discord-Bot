5022e0b0cb64e1058d9ae5b04f1273f882870b8f
Brandon Niles <brandonniles00@gmail.com>
2021-09-09 00:58:39 -0400

    Revamp of SQLite logging system.

--- SQLite.py
+++ SQLite.py
@@ -5,36 +5,54 @@ import datetime
 
 db_directory = "db"
 
+def openConn():
+    conn = sqlite3.connect('db/server_main.db')
+    c = conn.cursor()
+    return conn, c
+
+def closeConn(conn):
+    conn.commit()
+    conn.close()
+
+def createTables():
+    #Create a Table
+    conn, c = openConn()
+    c.execute("""CREATE TABLE IF NOT EXISTS messages(
+        username text,
+        user_id integer,
+        message_text text,
+        message_id integer,
+        channel text,
+        timestamp text
+    )""")
+    closeConn(conn)
+
 def dbInit():
     os.chdir(os. getcwd())
     if not os.path.isdir(db_directory):   
         os.mkdir(db_directory)
     createTables()
+    print("Database initialized successfully.")
 
-def createTables():
-    #Create a Table
-    c.execute("""CREATE TABLE if not exists messages (
-        username text,
-        user_id text,
-        message_text text,
-        message_id integer,
-        timestamp text
-    )""")
 
 def logMessage(message):
-    print("Logging message.")
-    unix = time.time()
-    timestamp = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M%S'))
+    conn, c = openConn()
     username = message.author.name
     user_id = message.author.id
     message_text = message.content
     message_id = message.id
-    c.execute("INSERT INTO messages VALUES (?, ?, ?, ?, ?)",
-    (username, user_id, message_text, message_id, timestamp))
-    conn.commit
-    print("Message logged.")
+    channel = message.channel.name
+    unix = time.time()
+    timestamp = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
+    c.execute("INSERT INTO messages VALUES (?,?,?,?,?,?)",
+    (username, user_id, message_text, message_id, channel, timestamp))
+    closeConn(conn)
 
-conn = sqlite3.connect('db/server_main.db')
-c = conn.cursor()
+def printDB():
+    conn, c = openConn()
+    c.execute("SELECT rowid, * FROM messages")
+    print(c.fetchall())
+    closeConn(conn)
 
+print("Attempting to start database...")
 dbInit()
