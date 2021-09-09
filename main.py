ef197f035186ffa19d1ce8db9127d485adf389d5
Brandon Niles <brandonniles00@gmail.com>
2021-09-08 23:13:06 -0400

    Hide token

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix = ',')
 
+
+print("Attempting to start bot...")
 @client.event
 async def on_ready():
     print("Bot initialized.")
 
-client.run('ODg1MzUyNjYxNDkzMzY2ODI0.YTly6w.QkMXYOQDdMyavb647W2gRh-oqzI')
+
+token = open("token.txt", "r").read()
+client.run(token)
