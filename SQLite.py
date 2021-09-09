e8867c10f03d2c6eb15408d42737f4817bef04c0
Brandon Niles <brandonniles00@gmail.com>
2021-09-09 00:06:35 -0400

    Message logging v 1.0

--- SQLite.py
+++ SQLite.py
@@ -1,5 +1,7 @@
 import sqlite3
 import os
+import time
+import datetime
 
 db_directory = "db"
 
@@ -18,6 +20,19 @@ def createTables():
         message_id integer,
         timestamp text
     )""")
+
+def logMessage(message):
+    print("Logging message.")
+    unix = time.time()
+    timestamp = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M%S'))
+    username = message.author.name
+    user_id = message.author.id
+    message_text = message.content
+    message_id = message.id
+    c.execute("INSERT INTO messages VALUES (?, ?, ?, ?, ?)",
+    (username, user_id, message_text, message_id, timestamp))
+    conn.commit
+    print("Message logged.")
 
 conn = sqlite3.connect('db/server_main.db')
 c = conn.cursor()
--- main.py
+++ main.py
@@ -5,13 +5,23 @@ from SQLite import *
 
 client = commands.Bot(command_prefix = ',')
 
-@client.event
-async def on_ready():
-    print("Bot initialized.")
+#Methods
 
 def startBot(token):
     print("Attempting to start bot...")
     client.run(token)
+
+
+#Events
+
+@client.event
+async def on_ready():
+    print("Bot initialized.")
+
+@client.event
+async def on_message(message):
+    print("Attempting to log message.")
+    logMessage(message)
 
 bot_token = open(token_file, "r").read() #Work on encryption later
 startBot(bot_token) #Must be last line
