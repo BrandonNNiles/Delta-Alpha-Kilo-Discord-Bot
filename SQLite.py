cf53c6880bb799b9953c0f21380b9c197b12321e
Brandon Niles <brandonniles00@gmail.com>
2021-09-08 23:48:06 -0400

    SQL organized into startup functions,

--- SQLite.py
+++ SQLite.py
@@ -1,5 +1,25 @@
 import sqlite3
+import os
+
+db_directory = "db"
+
+def dbInit():
+    os.chdir(os. getcwd())
+    if not os.path.isdir(db_directory):   
+        os.mkdir(db_directory)
+    createTables()
+
+def createTables():
+    #Create a Table
+    c.execute("""CREATE TABLE if not exists messages (
+        username text,
+        user_id text,
+        message_text text,
+        message_id integer,
+        timestamp text
+    )""")
 
 conn = sqlite3.connect('db/server_main.db')
-
 c = conn.cursor()
+
+dbInit()
